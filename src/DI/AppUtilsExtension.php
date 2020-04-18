3931a4f7514382d95581b8c4d6e2bbc64f7ea356
Pavel Lauko <pavel.lauko@bsc-ideas.com>
2020-04-18 23:44:03 +0200

    Nette 3 supported

--- src/DI/AppUtilsExtension.php
+++ src/DI/AppUtilsExtension.php
@@ -3,38 +3,38 @@
 namespace XRuff\App\Model\Utils\DI;
 
 use Nette;
-use XRuff\App\Model\Utils\Caching;
+use Nette\DI\Extensions\InjectExtension;
+use XRuff\App\Model\Utils;
 
 class AppUtilsExtension extends Nette\DI\CompilerExtension
 {
 	public function loadConfiguration()
 	{
 		$builder = $this->getContainerBuilder();
-		$config = $this->getConfig();
 
 		$builder->addDefinition($this->prefix('cache'))
-			->setClass(Caching\Cache::class, ['@cacheStorage', 'XRuff.AppUtils'])
-			->setInject(false);
+			->setFactory(Utils\Caching\Cache::class, ['@cacheStorage', 'XRuff.AppUtils'])
+			->addTag(InjectExtension::TAG_INJECT);
 
 		$builder->addDefinition($this->prefix('email'))
-			->setClass('XRuff\App\Model\Utils\Email')
-			->setInject(false);
+			->setFactory(Utils\Email::class)
+			->addTag(InjectExtension::TAG_INJECT);
 
 		$builder->addDefinition($this->prefix('logger'))
-			->setClass('XRuff\App\Model\Utils\Logger')
-			->setInject(false);
+			->setFactory(Utils\Logger::class)
+			->addTag(InjectExtension::TAG_INJECT);
 
 		$builder->addDefinition($this->prefix('settings'))
-			->setClass('XRuff\App\Model\Utils\Settings')
-			->setInject(false);
+			->setFactory(Utils\Settings::class)
+			->addTag(InjectExtension::TAG_INJECT);
 
 		$builder->addDefinition($this->prefix('repository.domain'))
-			->setClass('XRuff\App\Model\Utils\Repositories\DomainRepository')
-			->setInject(false);
+			->setFactory(Utils\Repositories\DomainRepository::class)
+			->addTag(InjectExtension::TAG_INJECT);
 
 		$builder->addDefinition($this->prefix('repository.settings'))
-			->setClass('XRuff\App\Model\Utils\Repositories\SettingsRepository')
-			->setInject(false);
+			->setFactory(Utils\Repositories\SettingsRepository::class)
+			->addTag(InjectExtension::TAG_INJECT);
 	}
 
 	/**
